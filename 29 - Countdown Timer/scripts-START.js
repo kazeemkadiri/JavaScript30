597a6340f05204b0a688248239f9764d8dbd7a6a
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2019-09-07 18:46:39 +0100

    Important updates on countdown-timer project

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,36 +1,27 @@
 //Add event listener to all timer buttons
-const TimerModule = (()=>{
+(()=>{
 	
 	const timerCount = {
 		seconds: 0,
-		minutes: 0,
-		time: 0,
-		timerInterval: null
+		minutes: 0
 	};
 	
-const addTimerButtonListeners = () => {
-  
-  document.querySelectorAll(".timer__button").forEach(function(timerButton){
-	
-  const time = timerButton.dataset.time;
+function addTimerButtonListeners(){
+		 document.querySelectorAll(".timer__button").forEach(function(timerButton){
+		 const time = timerButton.dataset.time;
+		 	timerButton.addEventListener('click',	function(){
+		 		timerCount.time = time;
+		 		startTimer(); 	
+		 		});//End timer event listener
+		 	
+		 });
+	};
 
-  timerButton.addEventListener('click',	() => {
-	  
-	  timerCount.time = time;
-
-	  startTimer(); 	
-	});//End timer event listener
-	
-  });
-};
-
-const decrementTimeCount = () => {
+function decrementTimeCount(){
 	
 	if(timerCount.seconds === 0){
 		
-		if(timerCount.minutes > 0){
-		  timerCount.minutes -= 1;
-		}
+		timerCount.minutes -= 1;
 		
 		timerCount.seconds = 60;
 		
@@ -40,93 +31,37 @@ const decrementTimeCount = () => {
 	
 }
 
-const updateDOMTimeCount = () => {
-
-	const { minutes, seconds } = timerCount;
-
-	document.querySelector('.display__time-left').textContent 
-	= `${(minutes < 10 ? '0' + minutes : minutes )} : ${seconds < 10 ? '0' + seconds : seconds}`;
-
+function updateDOMTimeCount(){
+	document.querySelector('.display__time-left').textContent = `${(timerCount.minutes)}
+	:${timerCount.seconds}`;
 }
 
-const resetTimerCountValues = () => {
-	timerCount.minutes = 0;
-	timerCount.seconds = 0;
-}
-
-const startTimer = () => {
-	
-	let { time, timerInterval } = timerCount;
-
-	if(parseInt(timerInterval)){
-
-	  clearInterval(timerInterval);
-
-	}
-	
-	  timerCount.minutes = (time < 60) ? 0: (time / 60).toFixed(0); 
+function startTimer(){
 	 
-	  timerCount.seconds = (timerCount.time % 60);
+	 const timerButtonMinutes = (timerCount.time / 60).toFixed(0); 
 	 
-
-	  timerCount.timerInterval = setInterval(() => {
-
-	    updateDOMTimeCount();
-
-		  if((timerCount.minutes === 0) && (timerCount.seconds === 0)){
-		
-		    clearInterval(timerInterval);
+	 const timerButtonSeconds = (timerCount.time % 60);
+	 
+		const timerInterval = setInterval(function(){
+			if((timerButtonMinutes === timerCount.minutes)
+				&& (timerButtonSeconds === timerCount.seconds)){
+					clearInterval(timerInterval);
+				}
 				
-		    resetTimerCountValues();
-			  
-			return;
-
-		  }
-
-		  decrementTimeCount();
+			decrementTimeCount();
 			
-		},1000);
-		
-}
-
-const AddcustomTimeInputListener = () => {
-
-  document.querySelector('input[name="minutes"]').addEventListener('keydown', e => {
-	
-    if( e.key.toUpperCase() === "ENTER" ){
-
-	  timerCount.time = e.target.value;
-
-	  startTimer();
+			updateDOMTimeCount();
+			
+		},100);
 	}
-
-  });
-
-}
-
-const preventSubmit = () => {
-
-  document.querySelector('#custom').addEventListener('submit', e => {
 	
-	e.preventDefault();
-
-  });
-
-}
-
-function init(){		
-	addTimerButtonListeners();
-	AddcustomTimeInputListener();
-}
-
-return {
-	init,
-	preventSubmit
-};
-
-})();
-
-TimerModule.init();
-TimerModule.preventSubmit();
+function init(){		addTimerButtonListeners();
+	}
+	
+	init();
+	
+})()
+//write function to run and display timer in DOM
+//COMMIT
 //Add listener for custom time input
 //Link to function to display timer

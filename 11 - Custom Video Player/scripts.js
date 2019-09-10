2fadc6219f2d992706a6a10ecc52350a44332660
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2019-09-10 17:09:21 +0100

    Added functionality for the volume and playback rate range element

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -9,6 +9,8 @@ const videoElement =  useDOMElement('.player__video');
 
 const skipButtons = useDOMElement('.player__button', true); //true indicates multiple element selection
 
+const sliderElements = useDOMElement('.player__slider', true);
+
 const toggleVideoPlay = () => {
   if(videoElement.paused){
     videoElement.play();
@@ -23,12 +25,25 @@ const skip = (e) => {
 	
 }
 
+const handleRangeUpdate = (e) => {
+
+		videoElement[e.target.name] = e.target.value;
+
+}
+
 const initListeners = () => {
   togglePlayButton.addEventListener('click', toggleVideoPlay);
   
   skipButtons.forEach( skipButton => {
   		skipButton.addEventListener('click', skip);
+  		
   });
+ 
+ sliderElements.forEach( sliderElement => {
+ 	sliderElement.addEventListener('change', handleRangeUpdate);
+ 	
+ });
+ 
   }
 
 

8f2aa0fec94728819d8594a15805516ec0e25356
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2019-09-10 12:49:18 +0100

    Added functionality for the skip buttons of the media player, to skip video by specified number of seconds

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,11 +1,13 @@
 
-const useDOMElement = (selector) => {
-	return document.querySelector(selector);
+const useDOMElement = (selector, multiple = false) => {
+	return multiple ? document.querySelectorAll(selector) : document.querySelector(selector);
 }
 
 const togglePlayButton = useDOMElement('.player__buttontoggle');
 	
 const videoElement =  useDOMElement('.player__video');
+
+const skipButtons = useDOMElement('.player__button', true); //true indicates multiple element selection
 
 const toggleVideoPlay = () => {
   if(videoElement.paused){
@@ -15,8 +17,18 @@ const toggleVideoPlay = () => {
   videoElement.pause();
 }
 
+const skip = (e) => {
+		
+	videoElement.currentTime += parseFloat(e.target.dataset.skip);
+	
+}
+
 const initListeners = () => {
   togglePlayButton.addEventListener('click', toggleVideoPlay);
+  
+  skipButtons.forEach( skipButton => {
+  		skipButton.addEventListener('click', skip);
+  });
   }
 
 
